d1e514aeb07254a9e9599ac8d56eeb14673d5378
lyy <wb-lyy569420@alibaba-inc.com>
2021-02-04 19:47:32 +0800

    feat: init

---
+++ typings/app/controller/index.d.ts
@@ -0,0 +1,15 @@
+// This file is created by egg-ts-helper@1.25.8
+// Do not modify this file!!!!!!!!!
+
+import 'egg';
+import ExportHome from '../../../app/controller/home';
+import ExportListIndex from '../../../app/controller/list/index';
+
+declare module 'egg' {
+  interface IController {
+    home: ExportHome;
+    list: {
+      index: ExportListIndex;
+    }
+  }
+}
